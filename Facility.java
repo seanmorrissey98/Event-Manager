846cf7e31199018df51470f801b77189821e25e3
Tomk1111 <tomk.tk53@gmail.com>
2018-04-08 15:22:04 +0100

    Update Facility.java

--- Facility.java
+++ Facility.java
@@ -1,85 +1,51 @@
 import java.util.*;
-import java.text.*;
-import javax.swing.*;
 public class Facility
 {
 	public int facilityId;
 	public String facilityName;
 	public double pricePerHour;
-	public static Date decommissionedUntilDate;
-	public static boolean availability;
+	public Date decommissionedUntil;
 	
-	public Facility(int facilityId,String facilityName,double pricePerHour,Date decommissionedUntilDate,boolean availability)
+	public Facility(int facilityId, String facilityName, double pricePerHour)
 	{
-		facilityId++;
-		this.facilityName=facilityName;
-		this.pricePerHour=pricePerHour;
-		this.decommissionedUntilDate=decommissionedUntilDate;
-		this.availability=availability;
+		this.facilityId 		 = facilityId;
+		this.facilityName		 = facilityName;
+		this.pricePerHour 		 = pricePerHour;
+		this.decommissionedUntil = decommissionedUntil;
+	}
+	
+	
+	public Facility(int facilityId, String facilityName, double pricePerHour, Date decommissionedUntil) //decommissionedUntil maybe as Date
+	{
+		this.facilityId 		 = facilityId;
+		this.facilityName 		 = facilityName;
+		this.pricePerHour 		 = pricePerHour;
+		this.decommissionedUntil = decommissionedUntil;
 	}
 	
 	public int getFacilityId()
 	{
-		return this.facilityId;
+		return facilityId;
 	}
 	
 	public String getFacilityName()
 	{
-		return this.facilityName;
+		return facilityName;
 	}
 	
 	public double getPricePerHour()
 	{
-		return this.pricePerHour;
+		return pricePerHour;
 	}
 	
-	public Date getDecommissionedUntilDate()
+	public Date getDecommissionedUntil()
 	{
-		return this.decommissionedUntilDate;
-	}
-	
-	public boolean getAvailability()
-	{
-		return this.availability;
-	}
-	
-	public void setFacilityId(int facilityId)
-	{
-		this.facilityId=facilityId;
-	}
-	
-	public void setFacilityName(String facilityName)
-	{
-		this.facilityName=facilityName;
-	}
-	
-	public void setPricePerHour(double pricePerHour)
-	{
-		this.pricePerHour=pricePerHour;
-	}
-	
-	public void setDecommissionedUntilDate(String decommissionedUntilDate)
-	{
-		try
-		{
-			SimpleDateFormat aFormatter=new SimpleDateFormat("dd/MM/yyyy");
-			this.decommissionedUntilDate=aFormatter.parse(decommissionedUntilDate);
-		}
-		catch(ParseException e)
-		{
-			JOptionPane.showMessageDialog(null,"Error when parsing data");
-		}
-	}
-	
-	public void setAvailability(boolean availability)
-	{
-		this.availability=availability;
-	}
+		return decommissionedUntil;
+	}	
 	
 	public String facilityToString()
 	{
-		String info=""+facilityId+","+facilityName+","+pricePerHour+","+decommissionedUntilDate;
+		String info	= facilityId + "," + facilityName + "," + pricePerHour + "," + decommissionedUntil;
 		return info;
 	}
-	
 }
